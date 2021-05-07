f9d24c13da40abc4e156f20ff6bb7e0b3a01c860
Hieu <hcn6@case.edu>
2021-05-07 07:01:54 +0700

    Implement class function closure and finish interpreting class closure

--- test.rkt
+++ test.rkt
@@ -1,30 +1,25 @@
-function divide(x, y) {
-  if (y == 0)
-    throw 1000000;
-  return x / y;
+class A {
+  var x = 6;
+  var y = 7;
+
+  function prod() {
+    return this.x * this.y;
+  }
+
+  function set2(a, b) {
+    x = a;
+    y = b;
+  }
 }
 
- function main() {
-  var x = 0;
-  var j = 1;
+class B extends A {
+  function set1(a) {
+    set2(a, a);
+  }
 
-  try {
-    while (j >= 0) {
-    var i = 10;
-    while (i >= 0) {
-      try {
-        x = x + divide(10*i, i);
-      }
-      catch(e) {
-        x = x + divide(e, j);
-      }
-      i = i - 1;
-    }
-    j = j - 1;
-   }
+  static function main () {
+    var b = new B();
+    b.set1(10);
+    return b.prod();
   }
-  catch (e2) {
-    x = x * 2;
-  }
-  return x;
 }
